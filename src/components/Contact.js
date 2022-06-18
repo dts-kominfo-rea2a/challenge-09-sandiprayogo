8269b0f95fb82e6108e82dd377322b836f55f5ba
sandiprayogo <prayogosandi67@gmail.com>
2022-06-18 14:39:42 +0700

    challenge-09

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,20 @@
 // Terima props lalu tampilkan dalam Contact component
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
-import React from 'react';
+import React from "react";
 
-const Contact = () => {
-    return (
-        <></>
-    )
-}
+const Contact = ({ data }) => {
+  return (
+    <div style={{ display: "flex", flexDirection: "row", backgroundColor: "#f4deb3", borderRadius: "10px", width: "30%", height: "auto", margin: "0 auto", marginBottom: "20px" }}>
+      <div style={{ width: "50%", display: "flex", verticalAlign: "middle", alignItems: "center", justifyContent: "center" }}>
+        <img src={data.photo} alt="" style={{ borderRadius: "50%", width: "40%" }} />
+      </div>
+      <div style={{ width: "50%", margin: "0 auto" }}>
+        <h4>{data.name}</h4>
+        <p>{data.phone}</p>
+        <p>{data.email}</p>
+      </div>
+    </div>
+  );
+};
 
 export default Contact;
