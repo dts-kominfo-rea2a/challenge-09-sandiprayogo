fb38e086dc2a9ae15bda14bcdfe011ff90e03fae
sandiprayogo <prayogosandi67@gmail.com>
2022-06-18 16:26:04 +0700

    latihan-09

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,7 @@
 // Terima props lalu tampilkan dalam Contact component
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
 import React from "react";
-import "../components/Contact.css";
+import '../components/Contact.css'
 
 const Contact = ({ data }) => {
   return (
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 // Kalian bisa menambahkan CSS di src/components/Header.css
 import React from "react";
-import "../components/Header.css";
+import '../components/Header.css'
 
 const Header = () => {
   return (
